49d938dafa762900b2f0497f24d2fd066af6a263
Barros99 <jeanmeneses@gmail.com>
2019-06-07 09:06:20 -0300

    Criação de componentes e modificação do arquivo de banco

---
+++ frontend/src/components/Missao.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default class Missao extends React.Component<> {
+
+  render() {
+
+      return(
+        <div style={{marginTop: '30%'}}>
+          <h1> Missao </h1>
+        </div>
+      );
+  }
+}
