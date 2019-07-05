3b6b693e3a2994d06a9afd9e6369d127a9cd5cf5
Barros99 <jeanmeneses@gmail.com>
2019-07-05 13:00:26 -0300

    Melhorias no json

--- frontend/src/components/Leitor.js
+++ frontend/src/components/Leitor.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
-import Arquivo from '../noticia.json';
+import Arquivo from "../noticia.json";
+import { tsExternalModuleReference } from "@babel/types";
 
 export default class Leitor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Arquivo: []
+    };
+  }
+
+  componentDidMount() {
+    // fetch('noticia.json')
+    //   .then(resultado => resultado.json())
+    //   .then(dados => this.setState({Arquivo: dados}));
+  }
+
+  // function montaHtml(block, key) {
+  //   return console.log(block.type === "paragraph" ? "texto" : "video") + key;
+  // }
 
   render() {
-    return <div>
-        {Arquivo.map((dados, indice)=> {
-            return <p><div key={indice}>{dados.conteudo}</div></p>
-        })}
-    </div>;
+    //const {Arquivo} = this.state;
+    return (
+      <div>
+        {Arquivo.map((dados, indice) => (
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">{dados.titulo}</h3>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {dados.conteudo.version}
+                <br />
+                {dados.conteudo.blocks.map((block, chave) => {
+                  const link = block.data.embed;
+                  return (
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      {block.data.caption}
+                    </a>
+                  );
+                })}
+              </h6>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
