b74ed6e625facb8bffe88064aefd6c1f4001c21f
Barros99 <jeanmeneses@gmail.com>
2019-10-18 13:30:35 -0300

    Correção do menu (hamburguer) e ir para o topo

---
+++ frontend/src/components/GoTop.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class  GoTop extends React.Component {
+    state = {
+         intervalId: 0,
+         thePosition: false
+     }; 
+
+    componentDidMount() { 
+
+       document.addEventListener("scroll", () => { 
+           if (window.scrollY > 170) { 
+                this.setState({ thePosition: true }) 
+           } else { 
+               this.setState({ thePosition: false }) 
+           } 
+        }); 
+        window.scrollTo(0, 0); 
+        
+     } 
+
+    onScrollStep = () => { 
+        if (window.pageYOffset === 0){ 
+            clearInterval(this.state.intervalId); 
+        } 
+       window.scroll(0, window.pageYOffset - this.props.scrollStepInPx); 
+    }
+
+    scrollToTop = () => { 
+       let intervalId = setInterval(this.onScrollStep, this.props.delayInMs); 
+       this.setState({ intervalId: intervalId }); 
+    }
+
+     renderGoTopIcon() {
+        if (this.state.thePosition){
+            return (<div className="back-to-top" onClick={this.scrollToTop}>(^)</div>)
+        }
+    }
+
+     render() {
+            return this.renderGoTopIcon()
+     } 
+}
+
